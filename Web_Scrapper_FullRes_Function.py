93bbd790d632f00e775102aa551cf14276890d90
pramit46 <pramit46@yahoo.com>
2018-01-10 14:44:36 +0530

    Moved the common properties under global section

--- Web_Scrapper_FullRes_Function.py
+++ Web_Scrapper_FullRes_Function.py
@@ -8,11 +8,34 @@ import base64
 import os
 import re
 import json
+from datetime import datetime
+import pandas as pd
 
 
-###############################################Declare The Global Fields#####################################################
+###############################################Declare/Build The Global Fields#####################################################
 scheme='https'
-encode='utf-8'  
+encode='utf-8'
+proxy_user_id='User ID To Connect To Proxy Server'
+proxy_password= 'Base64 Encoded Password to Connect Proxy Server'
+proxy_server='Proxy Server Name'
+proxy_port='Proxy Server Port'     
+header={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+
+         
+#Decode the password string on the runtime
+proxy_url='https://'+proxy_user_id+':'+str(base64.b64decode(bytes(proxy_password, encode)))[2:-1]\
+        +'@'+proxy_server+':'+proxy_port
+    
+proxy = request.ProxyHandler({scheme:proxy_url})
+auth = request.HTTPBasicAuthHandler()
+opener = request.build_opener(auth, request.HTTPHandler, proxy) #also pass 'proxy' parameter if required.
+opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+request.install_opener(opener)
 
 #Define search term
 searchTerm = 'Kate Winslet'
@@ -29,24 +52,10 @@ root_domain= re.split('[/:.?=]+',url) ##separate the domain name
 
 #####################################################Connect The URL########################################################
 
-def connectToURL(url):    
-    proxy_user_id="<<User ID to connect to proxy>>"
-    proxy_password= "<<Encoded Password to connect to proxy>>"
-    proxy_server="<<Proxy Server Name>>"
-    proxy_port="<<Proxy Server Port>>"      
-    header={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'\
-        ' Chrome/43.0.2357.134 Safari/537.36'}
-   
-    #Decode the password string on the runtime
-    proxy_url='https://'+proxy_user_id+':'+str(base64.b64decode(bytes(proxy_password, encode)))[2:-1]\
-        +'@'+proxy_server+':'+proxy_port
-    
-     #Connect the URL and scrap the data
-    try:
-        proxy = request.ProxyHandler({scheme:proxy_url})
-        auth = request.HTTPBasicAuthHandler()
-        opener = request.build_opener(auth, request.HTTPHandler,proxy) #also pass 'proxy' parameter if required.
-        request.install_opener(opener)
+def connectToURL(url):
+  
+    #Connect the URL and scrap the data
+    try:        
         response = request.Request(url, headers=header) #set user agent in header to avoid 403 error
 
 #        proxyDict = {'https' : proxy_url}
@@ -88,7 +97,7 @@ def extractDataFromSoup(scrapped_data):
         links=soup.find_all(html_tag)
         for img_link in links:
             if('google' in root_domain):
-                #links_list.append(str(img_link.get(google_xml_block))) 
+                #links_list.append(str(img_link.get(google_html_block_thumbnail))) 
                 #WARNING: the above line fetches only the thumbnail resolution, 
                 #for the full resolution pictures, use the below block. 
                 for img in soup.findAll('div', {'class': google_html_class_full_res}):
@@ -115,7 +124,7 @@ def extractDataFromSoup(scrapped_data):
 def retrieveImageLinks(links_list):
     
     counter=0
-    parentFolderName='D:\\Medical\\'  
+    parentFolderName='D:\\tmp\\'  
     #build directory from the url being passed so as to have unique directories for each url
     directory=parentFolderName+root_domain[2] 
     #create a flat file to store the links. Put the links file into the respective link directory
@@ -132,37 +141,40 @@ def retrieveImageLinks(links_list):
             file.write('URL: '+url+'\n\n')
             
             for link in links_list:
-                counter+=1        
-                finallink=link  # Assign link to linkew in case you do not need any customization
-           
-                file.write('\n'+str(counter)+' *** ')            
-                #file.write('Link extracted is: '+link+'\n')
-                   
-                #this is specific for google search links
-                if(root_domain[2]=='google'):
-                    if(('http' in link) or ('https' in link) or ('ftp' in link)):
-                        finallink=link
-                    elif(link.startswith('//')):
-                        finallink=scheme+':'+link
-                    elif(link.startswith('/')):
-                        finallink=root_domain[0]+'://'+root_domain[1]+'.'+root_domain[2]+'.'+root_domain[3]+link 
+                if(counter<100):
+                    counter+=1        
+                    finallink=link  # Assign link to linkew in case you do not need any customization
 
-                #for all other urls
-                else:
-                    if(link.startswith('//')):
-                        finallink=scheme+':'+link
-                    elif(link.startswith('/')):
-                        if(url[-1]=='/'):
-                            finallink=url[:-1]+link #-1 removes the trailing / from the field URL so as to remove the duplicates
-                        else:
-                            finallink=url+link
+                    file.write('\n'+str(counter)+' *** ')            
+                    #file.write('Link extracted is: '+link+'\n')
+
+                    #this is specific for google search links
+                    if(root_domain[2]=='google'):
+                        if(('http' in link) or ('https' in link) or ('ftp' in link)):
+                            finallink=link
+                        elif(link.startswith('//')):
+                            finallink=scheme+':'+link
+                        elif(link.startswith('/')):
+                            finallink=root_domain[0]+'://'+root_domain[1]+'.'+root_domain[2]+'.'+root_domain[3]+link 
+
+                    #for all other urls
+                    else:
+                        if(link.startswith('//')):
+                            finallink=scheme+':'+link
+                        elif(link.startswith('/')):
+                            if(url[-1]=='/'):
+                                finallink=url[:-1]+link #-1 removes the trailing / from the field URL so as to remove the duplicates
+                            else:
+                                finallink=url+link
             
-                file.write(finallink)
+                    file.write(finallink)
                                                                
-                #print('---------------------------------------------------------------------------')
-                #print('Extraction Index: '+str(counter)+'\nlink: '+link+'\nfinallink: '+finallink)
-                #print('---------------------------------------------------------------------------')
-            
+                    print('---------------------------------------------------------------------------')
+                    print('Extraction Index: '+str(counter)+'\nlink: '+link+'\nfinallink: '+finallink)
+                    print('---------------------------------------------------------------------------')
+                else:
+                    continue
+
     except IOError as io:
         print('IO Error Occurred:\n'+str(io))
         pass
@@ -177,15 +189,16 @@ def retrieveImageLinks(links_list):
         
     return links_to_store_filename
 
-############################################################################################################################# 
+#############################################################################################################################    
 ###################################################Fetch The Images##########################################################
     
 def extractImages(links_to_store_filename):
     
     filenametokens=links_to_store_filename.split('\\')
     file_path='\\\\'.join(filenametokens[:-1])+'\\\\'
-    #file_path=file_path.strip() #remove any unnecessary characters
-            
+    file_path=file_path.strip() #remove any unnecessary characters
+    
+              
     counter=0
     try:        
         with open(links_to_store_filename) as link_file:
@@ -201,18 +214,17 @@ def extractImages(links_to_store_filename):
                         finallink=records[1]
                         image_file_path=file_path+(re.split('[/?=:,!@]+',finallink)[-1])
                         image_file_path=image_file_path.strip() #remove any unnecessary characters
-                        print("v1: "+image_file_path)
                         
                         if('.' not in image_file_path):
                             #counter is added to avoid any unneccessary removal of filenames 
                             #in case they end with any of /?=: and also to avoid duplicates 
                             #print('created file path: '+image_file_path+str(counter)+'.jpg')
-                            image_file_path=image_file_path+str(counter)+'.jpg'
-                            print("v2: "+image_file_path)
+                            image_file_path=image_file_path+str(counter)+'.jpg'                            
                                                  
-                        #finally retrieve the file
+                        print("v1: "+image_file_path)
+
+                        #finally retrieve the file                        
                         request.urlretrieve(finallink.strip(), image_file_path.strip())
-            
                         #file.write('Extracted Data is at: ' + file_path+'\n\n')
         
     except IOError as io:
@@ -228,7 +240,7 @@ def extractImages(links_to_store_filename):
         print('Done!!.. Closed All The Connections.. Good Bye!!! :)')
             
 ########################################################<<DONE>>#############################################################    
-###############################################Start Calling The Functions#####################################################
+###############################################Start Calling The Methods#####################################################
 
 if(__name__=="__main__"):
     
@@ -239,6 +251,6 @@ if(__name__=="__main__"):
     data=connectToURL(url)
     list=extractDataFromSoup(data)
     linkfilename=retrieveImageLinks(list)
-    extractImages(linkfilename)
+    extractImages(linkfilename) #'D:\\tmp\\google\\links_2018-01-10.txt') 
 
 ############################################################################################################################
