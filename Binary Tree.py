31cb5ac4f6ef6c91b5d8ed3cc06e5b473ea3d7b9
pramit46 <pramit46@yahoo.com>
2018-01-31 16:27:56 +0530

    initialized root as None

--- Binary Tree.py
+++ Binary Tree.py
@@ -8,7 +8,6 @@ class Node:
         self.l=None
         self.v=value
         
-
         
 class Tree:       
     def __init__(self):
@@ -29,53 +28,57 @@ class Tree:
             self.traverse(root.r)
             
     def insert(self,new_value,node):
-        print(node)
         if(node==None):
             node=Node(new_value)
             print("Node created with the value: "+str(node.v))
         else:
             current_val=node.v
+            print("new_value: "+str(new_value)+" current_val: "+str(current_val))
             if(new_value<=current_val):
                 print("new val is less than current, so going left")
                 if(node.l==None):                    
                     node.l=Node(new_value)
                     print("created new left node and put the value")
                 else:
+                    print("going one step left")
                     self.insert(new_value,node.l)
             elif(new_value>current_val):
                 print("new val is greater than current, so going right")  
                 if(node.r==None):
                     node.r=Node(new_value)
                     print("created new right node and put the value")
-                else:                    
+                else:  
+                    print("going one step right")
                     self.insert(new_value,node.r)
         return node
     
     def getroot(self,user_value):
             if(self.root==None):
-                print("root is none")
                 return self.insert(user_value,self.root)
             else:
                 return self.root
     
     def delete(self):
         self.root=None
+        
     
 if(__name__=="__main__"):
     tree=None
     tree=Tree()
+    root=None
     list=[10,12,5,30,'exit']
     for i in list:
         print("\nthe value is: "+str(i))
         #user_value=input("Please enter the value:")
         user_value=i        
         if(user_value == "exit"):
-            tree.traverse(tree.root)
+            tree.traverse(root)
             break
         else:        
-            if(tree.root==None):
-                tree.root=tree.getroot(user_value)
+            if(root==None):
+                print('root is empty')
+                root=tree.getroot(user_value)
             else:
                 print("now the root is not empty")
-                node=tree.insert(user_value,tree.root)
+                node=tree.insert(user_value,root)
     tree.delete()
