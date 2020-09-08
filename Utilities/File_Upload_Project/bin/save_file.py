c4a38973db0266a76a537b9fe4390674ff1632ba
Pramit <prambasu@cisco.com>
2020-09-08 18:18:36 +0530

    Added Customization

--- Utilities/File_Upload_Project/bin/save_file.py
+++ Utilities/File_Upload_Project/bin/save_file.py
@@ -10,7 +10,7 @@ from werkzeug.utils import secure_filename
 
 __version__ = "1.0"
 #This needs to be dynamically populated, instead of hard coding. But not a priority right now
-baseLocation= os.path.join('C:\\','Users','prambasu','Downloads','Personal Docs','python_projects','MyPython','File_Upload_Project')
+baseLocation= os.path.join('C:\\','Users','prambasu','Downloads','Personal Docs','python_projects','MyPython','My-Python-Project','Utilities','File_Upload_Project')
 uploadedFileLocation = os.path.join(baseLocation,'Storage')
 logFileLocation=  os.path.join(baseLocation,'Logs')
 templateLocation =  os.path.join(baseLocation,'templates')
@@ -23,6 +23,7 @@ logger.basicConfig(level=logger.DEBUG, format='%(asctime)s %(levelname)s %(messa
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
+#Validate if the file extensions fall in the allowed list
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -61,16 +62,18 @@ def index():
 #Route here in case of error
 @app.route('/error/<errorMessage>')
 def errorPage(errorMessage):
-    finalString=("Content-Type: text/html\n<html><body><p>Some Error Occurred. Please Retry. %s </p></body></html>" % errorMessage)
+    finalString=("<html><body><p>Some Error Occurred. Please Retry. <br><br> %s </p></body></html>" % errorMessage)
     return finalString
     
 #Route here in case of success    
 @app.route('/success/<filename>')
 def successPage(filename):
     #Just acknowledge the successful upload
-    finalString=("Content-Type: text/html\n<html><body><p>Upload of %s Successful</p></body></html>" % filename)
+    finalString=("<html><body><p>Upload of %s Successful</p></body></html>" % filename)
     return finalString
-
+    
+    #index= render_template("successPage.html").read().format(filename=filename)
+    #return 
     #Show/download the content of the uploaded file
     #return send_from_directory(uploadedFileLocation,filename)
 
@@ -84,29 +87,38 @@ def uploadFile():
     #request.files=''
     message= ""
     fileName=""
-    try:
-        if(request.files and len(request.files)>0):
-            fileitem = request.files['filename']
+    try:        
+        if(request.files and request.files['filename']):
+            fileitem = request.files['filename']            
             # Test if the file was uploaded
             if allowed_file(fileitem.filename):
                 # strip leading path from file name to avoid
                 # directory traversal attacks
                 #fileName = os.path.basename(fileitem.filename)
-                    
+                #fileName = os.path.basename(fileitem.filename.replace("\\", "/" )) #FOR UNIX
+                logger.debug("Filename is allowed")
                 #The following line achieves the same thing that the above line would do along with some extra functionality.
                 #Hence upgrading to the secure_filename
                 fileName = secure_filename(fileitem.filename)
-                #fileName = os.path.basename(fileitem.filename.replace("\\", "/" )) #FOR UNIX
+                logger.debug("File name after securing "+fileName)
                 fileitem.save(os.path.join(uploadedFileLocation, fileName))
-                message = 'The file "' + fileName + '" was uploaded successfully'
+                message = 'Congrats!!!... The file ' + fileName + ' was uploaded successfully. Click below to see the file you uploaded'
             else:
-                message='No file was uploaded. Did you upload a file with allowed extension??'
+                message='No file was uploaded. Did you upload a file with any of these allowed extensions: '+str(ALLOWED_EXTENSIONS)+'?'
+                logger.debug(message)
+                logger.debug("routing to errorPage0")
                 return redirect(url_for('errorPage'), errorMessage=message)
         else:
+            message="And Please Don't Act Drunk!!!.. You will have to upload a file before clicking on the button!!!! You know that, right? Now Go Back"
+            logger.debug(message)
+            logger.debug("routing to errorPage1")
             return redirect(url_for('errorPage', errorMessage=message))
     except:
+        logger.debug(message)
+        logger.debug("routing to errorPage2")
         return redirect(url_for('errorPage', errorMessage=message))
-        
+    logger.debug(message)
+    logger.debug("routing to successPage")
     return redirect(url_for('successPage', filename=fileName))
 
 if __name__ == '__main__':
